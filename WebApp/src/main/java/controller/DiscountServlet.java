9e0ce92f8a8844fbb2d1c3d8dc967406d440046e
Tominq <jay.17891@gmail.com>
2023-11-08 17:51:40 +0700

    chore!: first commit

---
+++ WebApp/src/main/java/controller/DiscountServlet.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/discount")
+public class DiscountServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String description = request.getParameter("description");
+        double listPrice = Double.parseDouble(request.getParameter("listPrice"));
+        double discountPercent = Double.parseDouble(request.getParameter("discountPercent"));
+
+        double discountAmount = listPrice * discountPercent * 0.01;
+        double discountPrice = listPrice - discountAmount;
+
+        request.setAttribute("description", description);
+        request.setAttribute("listPrice", listPrice);
+        request.setAttribute("discountPercent", discountPercent);
+        request.setAttribute("discountAmount", discountAmount);
+        request.setAttribute("discountPrice", discountPrice);
+
+        request.getRequestDispatcher("/display-discount.jsp").forward(request, response);
+    }
+}
---
+++ WebApp/src/main/webapp/display-discount.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AD
+  Date: 11/8/2023
+  Time: 5:47 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+<h1>Discount Result</h1>
+<p>
+    Product Description: ${description}<br>
+    Price: ${listPrice}<br>
+    Discount Percent: ${discountPercent}%<br>
+    Discount Amount: ${discountAmount}<br>
+    Discount Price: ${discountPrice}<br>
+</p>
+</body>
+</html>
